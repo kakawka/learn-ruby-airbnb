8418351c1c092bf01aaea8aba7edc5d56e508687
Veronika Komarova <szdmcrew@yandex.ru>
2020-04-27 13:28:16 +0300

    fix code review

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def create
     new_photo = params.require(:photo).permit(:photo, :room_id)
-    a = Photo.create(new_photo)
-    redirect_to "/rooms/#{params[:photo][:room_id]}"
+    Photo.create(new_photo)
+    redirect_to room_path(params[:photo][:room_id])
   end
 end
